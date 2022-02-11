f49bdae7c776b92a892fae587984a925d02c69c2
filipesaretta <filipessm124@gmail.com>
2022-02-11 19:38:20 -0300

    Add axios and miragejs

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,24 @@
+import { useEffect } from 'react';
 import { Container, Content } from './styles';
 
 import logo from '../../assets/logo.svg';
-
+import { api } from '../../services/api';
+import { NewTransactionModal } from '../NewTransactionModal';
 
 export function Header() {
-  return (
+  useEffect(() => {
+    api('/transactions').then((resp) => {
+      console.log(resp.data);
+    });
+  }, []);
 
+  return (
     <Container>
       <Content>
-
         <img src={logo} alt="Logo - DT Money" />
 
-        <button className='button'>
-          Nova transação
-        </button>
-
+        <NewTransactionModal />
       </Content>
     </Container>
-
-  )
-
+  );
 }
