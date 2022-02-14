f79202113bcc4765bd77c004fa4da516f8625a58
filipesaretta <filipessm124@gmail.com>
2022-02-13 21:40:21 -0300

    Add files to correct places

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,61 @@
+import { createServer, Model } from 'miragejs';
 import { useState } from 'react';
-import styled from 'styled-components';
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { GlobalStyle } from './styles/global';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: {
+        id: 1,
+        title: 'Exemplo',
+        value: 1000,
+        category: 'Hello',
+        type: 'deposit',
+        createdAt: new Date('2021-02-12 09:00:00'),
+      },
+    });
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => this.schema.all('transaction'));
+
+    this.post('/transactions', (schema, req) => {
+      const data = JSON.parse(req.requestBody);
+      return schema.create('transaction', data);
+    });
+  },
+});
 
 function App() {
-  return <div className="App" />;
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+  function handleOpenTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+  return (
+    <>
+      <GlobalStyle />
+      <Header onOpenNewTransactionalModal={handleOpenTransactionModal} />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseTransactionModal}
+      />
+      <Dashboard />
+    </>
+  );
 }
 
 export default App;
