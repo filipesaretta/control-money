0afdfe02a3d166dec59e1a69b9aa3384d269a297
Nedyc <dorigo.and@gmail.com>
2022-02-22 11:53:03 +0100

    Completes the login/logout lifecycle

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,32 @@
-import { Container, Content, Button } from "./styles";
+import { useCallback } from "react";
 import logo from "../../assets/logo.svg";
+import { userStore } from "../../stores/userStore";
+import { Container, Content, Button } from "./styles";
 
 interface HeaderProps {
   onOpenNewTransactionalModal: () => void;
 }
 
 export function Header({ onOpenNewTransactionalModal }: HeaderProps) {
+  const user = userStore((state: any) => state.user);
+
+  const onLogout = useCallback(() => {
+    userStore.setState((state: any) => {
+      state.user = null;
+    });
+  }, []);
+
   return (
     <Container className="p-3">
+      <div
+        className="d-flex justify-content-end text-white mx-auto"
+        style={{ maxWidth: "1120px" }}
+      >
+        {user && user.name && <div>{user.name}</div>}
+        <div className="btn btn-light ms-4" onClick={onLogout}>
+          Logout
+        </div>
+      </div>
       <Content>
         <img src={logo} alt="Logo - DT Money" />
         <Button type="button" onClick={onOpenNewTransactionalModal}>
